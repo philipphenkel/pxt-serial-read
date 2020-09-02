d16c63bca58c92c0251cfe67f314b9816fb224ce
Philipp Henkel <philipphenkel@users.noreply.github.com>
2020-09-02 23:55:47 +0200

    Update the documentation

--- serial-read.ts
+++ serial-read.ts
@@ -7,8 +7,11 @@ const enum SerialMode {
 namespace serial {
 
   /**
-  * Consumes a single character from the serial read buffer.
-  * @param mode the. eg: ASYNC
+  * Reads and returns a single character from the serial read buffer. 
+  * Returns -1 if the buffer is empty or an error occurred.
+  * 
+  * @param mode the read mode. eg: SerialMode.ASYNC
+  * 
   *            ASYNC - A character is read from the serial read buffer if available, if there
   *                    are no characters to be read, a value of zero is returned immediately.
   *
@@ -19,15 +22,14 @@ namespace serial {
   *            SYNC_SLEEP - A character is read from the serial read buffer if available, if there
   *                         are no characters to be read, the calling fiber sleeps
   *                         until there is a character available.
-  * @return  a character, or -1 if there are no characters in the buffer.
+  * 
   */
   //% shim=serial::readChar
   export function readChar(mode: SerialMode): number  { return -1 }
 
-  
+
   /**
-  * Returns the currently buffered number of characters in the serial read buffer.
-  * @return number characters available
+  * Returns the number of currently available characters in the serial read buffer.
   */
   //% shim=serial::available
   export function available(): number { return 0 }
